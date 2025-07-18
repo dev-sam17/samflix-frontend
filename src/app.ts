3580e8476e50ab3048d31eca0bcaa3573ecc9961
Saurav Kumar <codewizard.saurav@gmail.com>
2025-07-18 08:33:55 +0530

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -82,8 +82,8 @@ app.use(
   }
 );
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = parseInt(process.env.PORT || "3000", 10);
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
